da868072945baeed609809fb7cd6dbae821d6284
Annalisa Comin <annalisa.comin@hotmail.com>
2025-04-30 22:40:49 +0200

    added swagger comments

--- backend/src/routes/monkeyRoutes.ts
+++ backend/src/routes/monkeyRoutes.ts
@@ -49,30 +49,107 @@ const router = express.Router();
 
 router.get('/', getAllMonkeys);
 
+/**
+ * @swagger
+ * /v1/monkeys:
+ *   post:
+ *     summary: Create a new monkey
+ *     tags: [Monkeys]
+ */
 router.post('/', validateRequest(createMonkeySchema), createMonkey);
 
+/**
+ * @swagger
+ * /v1/monkeys/{id}:
+ *   delete:
+ *     summary: Delete a monkey by ID
+ *     tags: [Monkeys]
+ */
 router.delete('/:id', deleteMonkey);
 
+/**
+ * @swagger
+ * /v1/monkeys/{id}:
+ *   patch:
+ *     summary: Edit a monkey by ID
+ *     tags: [Monkeys]
+ */
 router.patch('/:id', validateRequest(createMonkeySchema), editMonkey);
 
+/**
+ * @swagger
+ * /v1/monkeys/locations:
+ *   get:
+ *     summary: Get all locations
+ *     tags: [Locations]
+ */
 router.get('/locations', getLocations);
 
+/**
+ * @swagger
+ * /v1/monkeys/locations:
+ *   post:
+ *     summary: Create a location
+ *     tags: [Locations]
+ */
 router.post('/locations', validateRequest(locationFormSchema), createLocation);
 
+/**
+ * @swagger
+ * /v1/monkeys/locations/{id}:
+ *   delete:
+ *     summary: Delete a location by ID
+ *     tags: [Locations]
+ */
 router.delete('/locations/:id', deleteLocation);
 
+/**
+ * @swagger
+ * /v1/monkeys/locations/{id}:
+ *   patch:
+ *     summary: Edit a location by ID
+ *     tags: [Locations]
+ */
 router.patch(
   '/locations/:id',
   validateRequest(locationFormSchema),
   editLocation
 );
 
+/**
+ * @swagger
+ * /v1/monkeys/routes/{sourceLocationId}:
+ *   get:
+ *     summary: Get routes for a given source location
+ *     tags: [Routes]
+ */
 router.get('/routes/:sourceLocationId', getRoutes);
 
+/**
+ * @swagger
+ * /v1/monkeys/routes:
+ *   post:
+ *     summary: Create a new route
+ *     tags: [Routes]
+ */
 router.post('/routes', validateRequest(routeFormSchema), createRoute);
 
+/**
+ * @swagger
+ * /v1/monkeys/routes/{startId}/{destId}:
+ *   delete:
+ *     summary: Delete a route by source and destination ID
+ *     tags: [Routes]
+ */
 router.delete('/routes/:startId/:destId', deleteRoute);
 
+/**
+ * @swagger
+ * /v1/monkeys/routes:
+ *   patch:
+ *     summary: Edit an existing route
+ *     tags: [Routes]
+ */
 router.patch('/routes', validateRequest(routeFormSchema), editRoute);
 
 /**
