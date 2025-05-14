28849060537a1389b6ba90d09441189a88971d3e
Annalisa Comin <annalisa.comin@hotmail.com>
2025-05-14 10:45:41 +0200

    moved middleware in src

--- backend/src/routes/monkeyRoutes.ts
+++ backend/src/routes/monkeyRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { validateRequest } from 'middleware/validateRequest.js';
+import { validateRequest } from 'src/middleware/validateRequest.js';
 import {
   createMonkeySchema,
   locationFormSchema,
