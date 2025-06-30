bc6ce7e11e87ab8ee1e3909ad8e3999187136983
Annalisa Comin <annalisa.comin@hotmail.com>
2025-07-01 01:44:20 +0200

    fixed imports

--- backend/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import JourneyService from './journeyService.js';
-import LocationService from './locationService.js';
-import MonkeyService from './monkeyService.js';
-import RouteService from './routeService.js';
-
-export { JourneyService, LocationService, MonkeyService, RouteService };
